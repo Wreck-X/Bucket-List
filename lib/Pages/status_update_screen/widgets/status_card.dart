569c4bdf178d851a2cb8ffe0d8b29b5a7b32b6ee
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-07-08 18:35:23 +0530

    Add: status_card, status_floating_buttons. status_server_banner and server_screen dart files

---
+++ lib/Pages/status_update_screen/widgets/status_card.dart
@@ -0,0 +1,1 @@
+//Assignee Ivin (use models btw)
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
 }
 
 class MyApp extends StatelessWidget {
